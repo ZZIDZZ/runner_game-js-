d18e421fe97668d885e99fa6c793e86a1fe934bd
Zidan Utomo <78781000+ZZIDZZ@users.noreply.github.com>
2025-03-13 20:54:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ var shooting = false;
 var crouching = false;
 var jumpSpeedBuffer;
 var onAir = false;
-var playerSpeed = 3;
+var playerSpeed = 10;
 var obstacleInterval = 200;
 var groundPos = 300;
 var gameOver = false;
